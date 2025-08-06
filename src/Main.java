da92f7753dab9fe18a158270550be3aa5c915a2b
lisaemesowum <lisaemesowum8@gmail.com>
2025-08-06 21:50:41 +0100

    first commit: added java files and .gitignore

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+
+        //THIS IS MY FIRST JAVA PROGRAM
+        System.out.println("hello lisa!");
+        System.out.println("i like pizza is really Good");
+        System.out.println("buy me pizza");
+//        sout to auto generate this system.out.println
+        System.out.println("hello i am learning java");
+    }
+}
